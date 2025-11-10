82960816d11d89d183e7afc69ae5958a87a72c55
Divyesh Vekariya <divyeshkvekariya@gmail.com>
2025-11-10 13:07:16 +0530

    Initial Commit

---
+++ QR scanner/QR_scannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_scannerApp.swift
+//  QR scanner
+//
+//  Created by DK on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct QR_scannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QR scannerTests/QR_scannerTests.swift
@@ -0,0 +1,16 @@
+//
+//  QR_scannerTests.swift
+//  QR scannerTests
+//
+//  Created by DK on 10/11/25.
+//
+
+import Testing
+
+struct QR_scannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
